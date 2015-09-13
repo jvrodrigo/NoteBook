312401e5a1ef66621ec225d7de5ba7a125ef42c6
jvrodrigo <josevicenterodrigodelgado@gmail.com>
2015-09-13 14:00:40 +0200

    Changes

--- notebook/common/functions.php
+++ notebook/common/functions.php
@@ -2,9 +2,9 @@
 
 function guardarPost($title=null,$text=null){
 	//Conexion con la base
-	mysql_connect('mysql.hostinger.es','u698943981_nbook','456rtyfghvbn');
+	mysql_connect('mysql.default_host','user_name','password');
 //selección de la base de datos con la que vamos a trabajar
-	mysql_select_db("u698943981_nbook");
+	mysql_select_db("data_base_name");
 //Ejecucion de la sentencia SQL
 	mysql_query("insert into POSTS (title,text) values ('$title','$text')");
 
